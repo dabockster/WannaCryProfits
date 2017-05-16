31e6ce14bdea177278a6d33221fe656f421db9cb
Steven Bock <steven@stevenbock.me>
2017-05-16 14:09:02 -0700

    Valid data is being displayed. Total USD calculated correctly!

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <br>
 
     <?php
+			//all known BTC addresses associated with WannaCry
       $addresses = array(
         "12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw",
         "115p7UMMngoj1pMvkpHijcRdfJNXj6LrLn",
@@ -19,32 +20,66 @@
         "15zGqZCTcys6eCjDkE3DypCjXi6QWRV6V1"
       );
 
-      
-    ?>
-    <div>
-      <span class="regularText">Address: </span>
-      <br>
-      <span class="btcAddress">{{@key}}</span>
-    </div>
-    <br>
-    <div>
-      <span class="regularText">Value (BTC): </span>
-      <br>
-      <span class ="btcValue">{{this}} BTC</span>
-    </div>
-    <br>
-    <div>
-      <span class="regularText">Value (USD): </span>
-      <br>
-      <span class="usdValue">$</span>
-    </div>
-    <br>
-    <br>
-    {{/each}}
+			//blockchain api URL
+      $btcAPI = "https://blockchain.info/multiaddr?active=";
 
-    <h3>Total BTC spent by WannaCry victims: {{totalBTC}} BTC</h3>
-    <h3>Current USD equivalent: <u>${{totalUSD}}</u></h3>
-    <p>* USD prices for BTC are obtained from <a href="https://coinbase.com">CoinBase</a>.</p>
+			//USD price URL
+			$usdAPI = "https://api.coinbase.com/v2/prices/spot?currency=USD";
 
+			//append addersses to blockchain API call
+			foreach($addresses as $addr){
+				$btcAPI = $btcAPI . $addr;
+				$btcAPI = $btcAPI . "|";
+			}
+
+			//remove extra "|" from blockchain API call
+			$btcAPI = rtrim($btcAPI, '|');
+
+			//make API call to blockchain
+			$btcRawData = file_get_contents($btcAPI);
+			$btcData = json_decode($btcRawData, true)['addresses'];
+
+			//make API call to coinbase price api
+			$usdRawData = file_get_contents($usdAPI);
+			$usdPrice = json_decode($usdRawData, true)['data']['amount'];
+
+			//create running total BTC
+			$totalBTC = 0;
+
+			for ($i = 0; $i < count($addresses); $i++){
+				//get total btc for currently selected address
+				$currentTotal = $btcData[$i]['total_received'];
+
+				//compute total USD value for selected address
+				$usdValue = $usdPrice * $currentTotal;
+
+				//add total for currently selected address to running total
+				$totalBTC = $totalBTC + $currentTotal;
+
+				//template HTML containing address stats
+				echo "<div>\n" .
+							"<span class=\"regularText\">Address: " . $btcData[$i]['address'] . "</span>\n" .
+						"</div>\n" .
+						"<br>\n" .
+						"<div>\n" .
+							"<span class=\"regularText\">Value (BTC): " . $currentTotal . "</span>\n" .
+						"</div>\n" .
+						"<br>\n" .
+						"<div>\n" .
+							"<span class=\"regularText\">Value (USD): " . $usdValue . "</span>\n" .
+						"</div>\n" .
+						"<br>\n" .
+						"<br>\n"
+						;
+	    }
+
+			//compute total USD value of total BTC count
+			$totalUSD = $totalBTC * $usdPrice;
+
+			//template HTML containing overall totals
+			echo "<h3>Total BTC spent by WannaCry victims: " . $totalBTC . " BTC</h3>\n" .
+					"<h3>Current USD equivalent: <u>$" . $totalUSD . "</u></h3>\n";
+		?>
+		<p>* USD prices for BTC are obtained from <a href="https://coinbase.com">CoinBase</a>.</p>
 	</body>
 </html>
