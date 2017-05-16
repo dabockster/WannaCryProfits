6a50797dc1909cf4b2ed0082c8cbe71663d366e0
Steven Bock <steven@stevenbock.me>
2017-05-15 22:44:16 -0700

    Slowly relearning PHP after almost three years of not using it

--- index.php
+++ index.php
@@ -10,7 +10,17 @@
     <p>Stats for each known BTC address associated with the <a href="https://en.wikipedia.org/wiki/WannaCry_ransomware_attack">WannaCry ransomware</a>:</p>
     <br>
 
-    {{#each addressData}}
+    <?php
+      $addresses = array(
+        "12t9YDPgwueZ9NyMgw519p7AA8isjr6SMw",
+        "115p7UMMngoj1pMvkpHijcRdfJNXj6LrLn",
+        "13AM4VW2dhxYgXeQepoHkHSQuy6NgaEb94",
+        "1QAc9S5EmycqjzzWDc1yiWzr9jJLC8sLiY",
+        "15zGqZCTcys6eCjDkE3DypCjXi6QWRV6V1"
+      );
+
+      
+    ?>
     <div>
       <span class="regularText">Address: </span>
       <br>
